f773a4e1b8d516f6500ba30dfd33d7ac7ce35fb2
salisbum <megan.a.salisbury@gmail.com>
2016-03-30 13:13:00 -0400

    before we break it

--- leaderboard.js
+++ leaderboard.js
@@ -27,31 +27,61 @@ var gameInfo = function(){
  ];
 };
 
+var gameData = gameInfo();
 // YOUR CODE HERE
 function team(name, rank, wins, losses) {
   this.name = name;
-  this.rank = rank;
-  this.wins = wins;
-  this.losses = losses;
+  this.rank = 0;
+  this.wins = 0;
+  this.losses = 0;
 }
 
 var teamName = function(array){
- var teams = [];
+ var allTeams = [];
  var uniqueTeams = [];
  for (i = 0; i < array.length; i++) {
-   teams.push(array[i].home_team);
-   teams.push(array[i].away_team);
+   allTeams.push(array[i].home_team);
+   allTeams.push(array[i].away_team);
  }
- for ( var i in teams ){
-   if( uniqueTeams.indexOf(teams[i]) === -1){
-     uniqueTeams.push(teams[i]);
+
+ for ( var i in allTeams ){
+   if( uniqueTeams.indexOf(allTeams[i]) === -1){
+     uniqueTeams.push(allTeams[i]);
    }
  }
- teams = [];
+
+ var teams = [];
  for (i = 0; i < uniqueTeams.length; i++) {
    teams.push(new team(name = uniqueTeams[i]));
  }
- console.log(teams);
+ return teams;
 };
 
-teamName(gameInfo());
+var teamObjects = teamName(gameInfo());
+
+var winsLosses = function(array){
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].home_score > array[1].away_score) {
+       for (var i = 0; i < teamObjects.length; i++) {
+         if (teamObjects[i].name === array[i].home_team) {
+           teamObjects[i].wins++;
+         } else {
+           teamObjects[i].losses++;
+         }
+       }
+    }
+  }
+  return teamObjects;
+};
+
+
+
+
+
+
+
+
+
+
+// console.log(teamObjects);
+console.log(winsLosses(gameData));
